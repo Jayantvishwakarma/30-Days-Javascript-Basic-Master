436be20aa2f819cb29de2e67ccbe3f114ee4dbc4
Jayant Vishwakarma <119335439+Jayantvishwakarma@users.noreply.github.com>
2023-09-27 17:03:37 +0000

    datatypes

---
+++ Basic/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  //treat all code as newer version
+// alert(3+3)
+console.log(3
+    +
+    3);
+
+console.log("jayant") //string is name like jay, naman etc
+let name ="jay"
+let age = 22          //number is 2 to power 53
+let isLoggedId = false //boolean is true and false
+let city = null        // null is a object value
+let home = undefined  // undefined
+
+console.log(typeof age);
+console.log(typeof null);
